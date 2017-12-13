595e8fac023497ab17ecef78fef594daf6c1c447
Tr1nks <tr1nks96@gmail.com>
2017-12-13 17:30:44 +0200

    + Faculty Entity

---
+++ src/main/java/com/tr1nksgroup/model/entities/FacultyEntity.java
@@ -0,0 +1,27 @@
+package com.tr1nksgroup.model.entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * факультет
+ * 6.[04].051.010.17.01
+ */
+@Entity
+@Table(name = "faculty")
+public class FacultyEntity {
+    /**
+     * шифр факультета
+     */
+    @Basic
+    @Column(name = "faculty_id", nullable = false, length = 3)
+    private int faculty;
+    /**
+     * название факультета
+     */
+    @Basic
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+}
